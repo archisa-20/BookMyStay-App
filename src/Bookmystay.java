230b0fbb761dd343a7a58491a9df5ce37ac0046b
archisa-20 <archisadas20@gmail.com>
2026-03-16 14:29:23 +0530

    UC2: Basic Room Types & Static Availability

--- src/Bookmystay.java
+++ src/Bookmystay.java
@@ -1,12 +1,87 @@
+/**
+ * Abstract Room class representing common properties of all room types.
+ */
+ abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Room Size: " + size + " sq.ft");
+        System.out.println("Price per Night: $" + price);
+    }
+}
+/**
+ * Represents a Single Room type.
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
+/**
+ * Represents a Double Room type.
+ */
+ class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+}
+/**
+ * Represents a Suite Room type.
+ */
+ class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 300);
+    }
+}
+/**
+ * Use Case 2: Basic Room Types & Static Availability
+ */
 public class Bookmystay {
+
     public static void main(String[] args) {
 
-        // Display welcome message
-        System.out.println("==================================");
-        System.out.println("      Welcome to Book My Stay     ");
-        System.out.println("     Hotel Booking Application    ");
-        System.out.println("==================================");
+        System.out.println("=================================");
+        System.out.println("      Book My Stay System        ");
+        System.out.println("         Version 2.1             ");
+        System.out.println("=================================\n");
 
-        System.out.println("Application started successfully.");
+        // Create room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability variables
+        int singleRoomAvailability = 10;
+        int doubleRoomAvailability = 5;
+        int suiteRoomAvailability = 2;
+
+        System.out.println("----- Available Room Types -----\n");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailability);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailability);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailability);
     }
 }
